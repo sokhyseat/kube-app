0c5ec357cb45d57adf026af0d50d7b26119cf155
unknown <sokhyseat8888@gmail.com>
2026-06-09 11:19:05 +0700

    second commit

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ app.get('/', (req, res) => {
   res.send(`
     <h1>Hello from this NodeJS app!</h1>
     <p>Try sending a request to /error and see what happens</p>
+    <p> this is the update<p/>
   `);
 });
 
